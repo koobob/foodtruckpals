d9384192825dd8f4a5b9b9fbc339332c6b6c9213
Bobby J <bobbyjung@gmail.com>
2023-10-12 16:04:10 -0700

    Enable phoenix to connect to db.
    Also, play with config for docker.

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ import Config
 
 # Configure your database
 config :food_truck_pals, FoodTruckPals.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  username: "foodtruckpals",
+  password: "foodtruckpals",
+  hostname: "db",
   database: "food_truck_pals_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
